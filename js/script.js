0b3d14a6625dc3f2283ae4cc8bf6cde14db63e89
Айкин Иван <ivan.aykin@mail.ru>
2023-06-08 18:46:25 +0700

    added burger menu

--- js/script.js
+++ js/script.js
@@ -4,9 +4,10 @@ user_icon.addEventListener("click", function () {
 	user__menu.classList.toggle('_active');
 });
 
-let header__menu = document.querySelector('.header__menu');
+let header__menu = document.querySelector('.icon-menu');
 header__menu.addEventListener("click", function () {
-	let
-		menu__icon = document.querySelector('.menu__icon');
+	let menu__icon = document.querySelector('.icon-menu');
+	let menu__body = document.querySelector('.menu__body');
 	menu__icon.classList.toggle('active');
+	menu__body.classList.toggle('active');
 });
